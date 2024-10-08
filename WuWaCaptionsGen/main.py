aa5df6f7109e4d75c5641a1a767340350002506c
Sunset <26019675+SunsetMkt@users.noreply.github.com>
2024-10-08 12:59:10 +0800

    Batch SRT Gen

--- WuWaCaptionsGen/main.py
+++ WuWaCaptionsGen/main.py
@@ -8,10 +8,7 @@ import os
 import pysrt
 
 ##### Config #####
-CgName = "C0002"  # The CgName of the subtitle you want to export.
-
-##### (Basically) Static #####
-CgFPS = 30  # The FPS of the video you want to export, usually 30 for WuWa.
+CgFPS = 30  # The FPS of the video you want to export, usually 30 for WuWa. Do NOT change it unless you know what you're doing.
 ShowMomentKey = "ShowMoment"  # ShowMomentEn etc. also available in some videos.
 DurationKey = "Duration"
 
@@ -56,38 +53,56 @@ def frame_to_ms(frame):
 VideoCaption = load_json("videocaption.json")
 MultiText = load_json("MultiText.json")
 
-# Filter captions
-DestCaption = []
-for caption in VideoCaption:
-    if caption["CgName"] == CgName:
-        DestCaption.append(caption)
 
-# Sort the list by CaptionId
-DestCaption.sort(key=lambda x: x["CaptionId"])
+def generate_caption(CgName):
+    # Filter captions
+    DestCaption = []
+    for caption in VideoCaption:
+        if caption["CgName"] == CgName:
+            DestCaption.append(caption)
 
-# Get the real caption text
-for caption in DestCaption:
-    caption["CaptionText"] = lookup_text(caption["CaptionText"])
+    # Sort the list by CaptionId
+    DestCaption.sort(key=lambda x: x["CaptionId"])
 
-# New subtitle
-new_srt = pysrt.SubRipFile()
+    # Get the real caption text
+    for caption in DestCaption:
+        caption["CaptionText"] = lookup_text(caption["CaptionText"])
 
-# Add captions
-count = 0
-for caption in DestCaption:
-    new_srt.append(
-        pysrt.SubRipItem(
-            index=count,
-            start=pysrt.SubRipTime(
-                milliseconds=frame_to_ms(caption[ShowMomentKey]),
-            ),
-            end=pysrt.SubRipTime(
-                milliseconds=frame_to_ms(caption[ShowMomentKey] + caption[DurationKey]),
-            ),
-            text=caption["CaptionText"],
+    # New subtitle
+    new_srt = pysrt.SubRipFile()
+
+    # Add captions
+    count = 0
+    for caption in DestCaption:
+        new_srt.append(
+            pysrt.SubRipItem(
+                index=count,
+                start=pysrt.SubRipTime(
+                    milliseconds=frame_to_ms(caption[ShowMomentKey]),
+                ),
+                end=pysrt.SubRipTime(
+                    milliseconds=frame_to_ms(
+                        caption[ShowMomentKey] + caption[DurationKey]
+                    ),
+                ),
+                text=caption["CaptionText"],
+            )
         )
-    )
-    count += 1
+        count += 1
 
-# Save
-save_srt(f"{CgName}.srt", new_srt)
+    # Save
+    save_srt(f"{CgName}.srt", new_srt)
+
+
+if __name__ == "__main__":
+    # Find all possible CgName
+    CgNameList = []
+    for Cg in VideoCaption:
+        if Cg["CgName"] not in CgNameList:
+            CgNameList.append(Cg["CgName"])
+
+    for CgName in CgNameList:
+        print(f"Generating {CgName}.srt")
+        generate_caption(CgName)
+
+    print("Done")
